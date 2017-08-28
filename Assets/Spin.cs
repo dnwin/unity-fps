0803b0bc14f3036a1e1e2af8d0bee1357bd121f0
DNwin <ndennis@gmail.com>
2017-08-28 01:16:22 -0700

    Maze scene, player mkeyboard movement, mouse camera and rotation

---
+++ Assets/Spin.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+	public float speed = 0.3f;
+
+	void Update () {
+		transform.Rotate (0, speed, 0);	
+	}
+}
